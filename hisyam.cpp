083b5352718cc596ff793201aa28b0a8cc88dcf1
Samsama21 <115509594+Samsama21@users.noreply.github.com>
2022-10-11 08:46:19 +0700

    Add files via upload

---
+++ hisyam.cpp
@@ -0,0 +1,17 @@
+// NAMA		: Muhammad Hisyam Herbiyan
+// NRP		: 2040221057
+// Jurusan	: Teknik Elektro Otomasi
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+float input;
+cin >> input;
+
+   cout << 62 << " ";
+    cout << setprecision(6) << 29.8998 << std::endl;
+}
